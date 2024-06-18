ec65e2d308a1ddacc23c2293a264a2f781c3062c
seth-way <seth.r.way@gmail.com>
2024-06-18 19:56:45 -0400

    add message to endRound to include durtion

--- src/round.js
+++ src/round.js
@@ -3,6 +3,7 @@ const createRound = deck => ({
   turns: 0,
   currentCard: deck[0],
   incorrectGuesses: [],
+  start: Date.now(),
 });
 
 const evaluateGuess = (guess, correctAnswer) => {
@@ -33,10 +34,27 @@ const calculatePercentCorrect = round => {
   return ~~((numCorrectGuesses / round.turns) * 100);
 };
 
+const calculateDuration = round => {
+  let seconds = ~~((Date.now() - round.start) / 1000);
+  let minutes = 0;
+
+  while (seconds > 59) {
+    seconds -= 60;
+    minutes += 1;
+  }
+
+  return [minutes, seconds];
+}
+
 const endRound = round => {
+  const [minutes, seconds] = calculateDuration(round);
   const percentageCorrect = calculatePercentCorrect(round);
-  const message = `** Round over! ** You answered ${percentageCorrect}% of the questions correctly!`;
+  
+  const message = `** Round over!\n** You answered ${percentageCorrect}% of the questions correctly!`;
   console.log(message);
+
+  const message2 = `** It took ${minutes} minutes & ${seconds} seconds!`;
+  console.log(message2);
 };
 
 module.exports = {
