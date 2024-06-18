7d5b77712c35bc871929293ef7f03ddb633abf31
seth-way <seth.r.way@gmail.com>
2024-06-18 12:17:59 -0400

    update start function for working console game

--- src/game.js
+++ src/game.js
@@ -1,14 +1,29 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const { createCard } = require('./card');
+const { createRound } = require('./round');
+const { createDeck, countCards } = require('./deck');
 
-function printMessage(deck) {
-  console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
+const printMessage = deck => {
+  console.log(`Welcome to FlashCards! You are playing with ${countCards(
+    deck
+  )} cards.
   -----------------------------------------------------------------------`);
-}
+};
 
-function printQuestion(round) {
+const printQuestion = round => {
   util.main(round);
-}
+};
 
-module.exports = { printMessage, printQuestion };
+const start = () => {
+  const cards = prototypeQuestions.map(card =>
+    createCard(...Object.values(card))
+  );
+  const deck = createDeck(cards);
+  printMessage(deck);
+  const round = createRound(deck);
+  printQuestion(round);
+};
+
+module.exports = { start, printMessage, printQuestion };
